7150384317a00a90f80477ffa392b27742aae320
theanh20041507 <120191900+theanh20041507@users.noreply.github.com>
2023-10-05 23:11:00 +0700

    Add files via upload
    happybirthday

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'HOÈN';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100040189193891';
+const eventName = 'HAPPY BIRTHDAY';
+const titleCard = 'GỬI THANH HUYỀN';
+const contentCard = 'Chúc mừng sinh nhật! Ngày này là dịp đặc biệt để tận hưởng niềm vui và sự kỷ niệm của cuộc đời bạn. Mong rằng những ước mơ sẽ thành hiện thực và hạnh phúc luôn bao quanh bạn trong mọi khoảnh khắc.';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = '57c0aa139df149af10e0.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
